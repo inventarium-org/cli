ba91283e52f07825f2c5f75c4f4984966e2e7317
Anton Davydov <antondavydov.o@gmail.com>
2020-05-02 23:41:58 +0300

    Create simple script for pushing service.yaml file

--- lib/inventarium/cli/push.rb
+++ lib/inventarium/cli/push.rb
@@ -1,5 +1,8 @@
 require 'tty-progressbar'
 require "pastel"
+require 'net/http'
+require 'yaml'
+require 'json'
 
 module Inventarium
   module CLI
@@ -11,17 +14,44 @@ module Inventarium
         "./meta/specific_service.yml   # Push specific_service.yml from './meta' folder"
       ]
 
+      DEFAULT_BASE_URL = 'http://localhost:2300'
+
       def call(args: [], **)
-        # dir = args.first || './service.yml'
         bar = TTY::ProgressBar.new("Puishing service.yaml to inventarium.io [:bar]", total: 30)
-        10.times do
-          sleep(0.1)
-          bar.advance(3)
-        end
+
+        path = args.first || './service.yml'
+
+        bar.advance(10)
+
+        config = parse_config(path)
+
+        bar.advance(10)
+
+        push_config(config)
+
+        bar.advance(10)
 
         pastel = Pastel.new
         puts "[#{pastel.green('DONE')}]"
         # puts "Push a new service.yml file from '#{dir}' directory"
+      end
+
+    private
+
+      def parse_config(path)
+        YAML.load_file(path)
+      end
+
+      def push_config(config)
+        base_url = ENV['INVENTARIUM_BASE_URL'] || DEFAULT_BASE_URL
+        url = URI("#{base_url}/api/services")
+
+        req = Net::HTTP::Post.new(url, 'Content-Type' => 'application/json')
+        req.body = { token: 'test', service: config }.to_json
+
+        Net::HTTP.start(url.hostname, url.port) do |http|
+          http.request(req)
+        end
       end
     end
   end
