c48de407cdab135a0dd4b56328d4ec9685e64e22
Anton Davydov <antondavydov.o@gmail.com>
2020-05-09 00:01:08 +0300

    Implement logic of init command

--- lib/inventarium/cli/init.rb
+++ lib/inventarium/cli/init.rb
@@ -6,20 +6,27 @@ module Inventarium
       desc "Create an empty service.yml file for the service"
 
       example [
-        "                # Generate service.yml in root folder",
-        "./.service_meta # Generate service.yml in '.service_meta' folder"
+        "                   # Generate service.yml in root folder",
+        "./.service_meta/   # Generate service.yml in '.service_meta' folder",
+        "order_service.yaml # Generate order_service.yml in root folder"
       ]
 
       def call(args: [], **)
         dir = args.first || '.'
 
         pastel = Pastel.new
-
         printf "Generating a new service.yaml\t\t"
-        sleep(0.3)
+
+        FileUtils.cp(template_file_path, dir)
         puts "[#{pastel.green('DONE')}]"
 
         # puts "Created a new service.yml file in '#{dir}' directory"
+      end
+
+    private
+
+      def template_file_path
+        File.expand_path(File.dirname(__FILE__) +  "/templates/service.yaml")
       end
     end
   end
